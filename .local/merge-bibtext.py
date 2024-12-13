b2271b926d81609771450ade3787307d34bcd987
Alberto Tonda <alberto.tonda@gmail.com>
2024-12-13 20:29:37 +0100

    Merge correcting errors with publications

--- .local/merge-bibtext.py
+++ .local/merge-bibtext.py
@@ -3,6 +3,7 @@ import os
 
 files = [f for f in os.listdir(".") if f.endswith(".bib")]
 #files = ["publications-journals.bib", "publications-conferences.bib"]
+#"invited" should not be considered, it's empty and creates issues
 print(files)
 
 output_file = "publications.bib"
