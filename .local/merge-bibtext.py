4512d2cb9fb432640d9f4d226597cb070799eacf
Alberto Tonda <alberto.tonda@gmail.com>
2024-12-13 19:54:39 +0100

    Publications removed, to be re-created

--- .local/merge-bibtext.py
+++ .local/merge-bibtext.py
@@ -2,7 +2,7 @@ import os
 # we really hope that keys are unique
 
 files = [f for f in os.listdir(".") if f.endswith(".bib")]
-files = ["publications-journals.bib", "publications-conferences.bib"]
+#files = ["publications-journals.bib", "publications-conferences.bib"]
 print(files)
 
 output_file = "publications.bib"
