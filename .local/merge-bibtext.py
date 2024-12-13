646520ea77c73f419355848fb2972b80a929acf5
Alberto Tonda <alberto.tonda@gmail.com>
2024-12-13 10:34:58 +0100

    Corrections to the automatically generated folders

---
+++ .local/merge-bibtext.py
@@ -0,0 +1,18 @@
+import os
+# we really hope that keys are unique
+
+files = [f for f in os.listdir(".") if f.endswith(".bib")]
+files = ["publications-journals.bib", "publications-conferences.bib"]
+print(files)
+
+output_file = "publications.bib"
+text = ""
+for f in files :
+    with open(f, "r") as fp :
+        text += fp.read()
+        text += "\n"
+    
+    
+with open(output_file, "w") as fp :
+    fp.write(text)
+    
