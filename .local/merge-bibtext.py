ee59f22acc3577abe45485b70a02ab044c3eaffd
Alberto Tonda <alberto.tonda@gmail.com>
2026-08-14 10:29:47 +0200

    Upload PDF, changes to publication merge script

--- .local/merge-bibtext.py
+++ .local/merge-bibtext.py
@@ -1,12 +1,12 @@
 import os
 # we really hope that keys are unique
 
-files = [f for f in os.listdir(".") if f.endswith(".bib")]
+output_file = "publications.bib"
+files = [f for f in os.listdir(".") if f.endswith(".bib") and f != output_file]
 #files = ["publications-journals.bib", "publications-conferences.bib"]
 #"invited" should not be considered, it's empty and creates issues
 print(files)
 
-output_file = "publications.bib"
 text = ""
 for f in files :
     print("Now reading file \"%s\"..." % f)
